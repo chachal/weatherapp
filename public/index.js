a71c68111a79714e07a5e66003c64685932091d6
chachal <charlotta.alen@gmail.com>
2018-01-22 15:10:28 +0200

    autocomplete works

--- public/index.js
+++ public/index.js
@@ -5,15 +5,21 @@ index.controller('IndexMap', function($scope) {
     $scope.name1 = '';
 });
 
-index.controller('IndexSearch', function($scope, $http, $q) {
+index.controller('IndexSearch', function($scope, $http, $q, $log) {
 
   $http.get('/api/locdata')
   .then(function(res, err) {
       $scope.locations = res.data
   });
 
-  $scope.searchText = '';
-
+  $scope.searchTextChange = function(searchtxt) {
+      $scope.matches = [];
+      $scope.locations.forEach( function(entry) {
+          if (entry.city.toLowerCase().indexOf(searchtxt) >= 0) {
+              $scope.matches.push(entry);
+          }
+      })
+  }
 
 
 });
